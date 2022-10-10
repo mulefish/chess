f054b3b6492033e643650b5d312da52accc533d6
pmontgomery <finch.wren.swift@gmail.com>
2022-10-10 13:49:46 -0700

    css is neat but fragil

--- static/game.js
+++ static/game.js
@@ -1,27 +1,132 @@
 
 
 const letters = ["a","b", "c", "d", "e","f","g","h"]
-function createUI() {
-    const eighth = window.innerHeight / 10
-    let table = "<table border='1'>"
-    let i = 0 
-    for ( let row = 0; row < 8; row++ ) {
-        table += "<tr>"
-        for ( let col = 0; col < 8; col++ ) {
-            const css = i % 2 === 0 ? "feltgreen" : "offwhite"
-            const ltr = `${row}${letters[col]}`
-            table += `<td>
-                <div class='${css}'>
-                ${ltr}
-                </div>
-            </td>`
-            i++
+
+class Cell {
+    constructor(row, col, css) {
+        this.letter = letters[col]
+        this.rc =  `${row}${letters[col]}`
+        this.row = row
+        this.col = col
+        this.css = css
+    }
+
+    getHtml() { 
+        let visibleNumber = "&nbsp;"
+        let visibleLetter = "&nbsp;"
+        if ( this.col === 0 ) {
+            visibleNumber = (this.row + 1 )
         }
-        i++
-        table += "</tr>"
+
+        if ( this.row === 7 ) {
+            visibleLetter = this.letter
+        }
+
+        let numberCss = ""
+        let letterCss = ""
+
+        if ( this.css === "feltgreen") {
+            numberCss = "whiteNumber"
+            letterCss = "whiteLetter"
+        } else {
+            numberCss = "greenNumber"
+            letterCss = "greenLetter"
+        }
+
+
+        return `<div class=${this.css}>
+            <div class='${numberCss}'>${visibleNumber}</div>
+            <div class='${letterCss}'>${visibleLetter}</div>
+        </div>`
+
+    }
+}
+
+let cells = {}
+cells['0a'] = new Cell(0, 0, 'feltgreen')
+cells['0b'] = new Cell(0, 1, 'offwhite')
+cells['0c'] = new Cell(0, 2, 'feltgreen')
+cells['0d'] = new Cell(0, 3, 'offwhite')
+cells['0e'] = new Cell(0, 4, 'feltgreen')
+cells['0f'] = new Cell(0, 5, 'offwhite')
+cells['0g'] = new Cell(0, 6, 'feltgreen')
+cells['0h'] = new Cell(0, 7, 'offwhite')
+cells['1a'] = new Cell(1, 0, 'offwhite')
+cells['1b'] = new Cell(1, 1, 'feltgreen')
+cells['1c'] = new Cell(1, 2, 'offwhite')
+cells['1d'] = new Cell(1, 3, 'feltgreen')
+cells['1e'] = new Cell(1, 4, 'offwhite')
+cells['1f'] = new Cell(1, 5, 'feltgreen')
+cells['1g'] = new Cell(1, 6, 'offwhite')
+cells['1h'] = new Cell(1, 7, 'feltgreen')
+cells['2a'] = new Cell(2, 0, 'feltgreen')
+cells['2b'] = new Cell(2, 1, 'offwhite')
+cells['2c'] = new Cell(2, 2, 'feltgreen')
+cells['2d'] = new Cell(2, 3, 'offwhite')
+cells['2e'] = new Cell(2, 4, 'feltgreen')
+cells['2f'] = new Cell(2, 5, 'offwhite')
+cells['2g'] = new Cell(2, 6, 'feltgreen')
+cells['2h'] = new Cell(2, 7, 'offwhite')
+cells['3a'] = new Cell(3, 0, 'offwhite')
+cells['3b'] = new Cell(3, 1, 'feltgreen')
+cells['3c'] = new Cell(3, 2, 'offwhite')
+cells['3d'] = new Cell(3, 3, 'feltgreen')
+cells['3e'] = new Cell(3, 4, 'offwhite')
+cells['3f'] = new Cell(3, 5, 'feltgreen')
+cells['3g'] = new Cell(3, 6, 'offwhite')
+cells['3h'] = new Cell(3, 7, 'feltgreen')
+cells['4a'] = new Cell(4, 0, 'feltgreen')
+cells['4b'] = new Cell(4, 1, 'offwhite')
+cells['4c'] = new Cell(4, 2, 'feltgreen')
+cells['4d'] = new Cell(4, 3, 'offwhite')
+cells['4e'] = new Cell(4, 4, 'feltgreen')
+cells['4f'] = new Cell(4, 5, 'offwhite')
+cells['4g'] = new Cell(4, 6, 'feltgreen')
+cells['4h'] = new Cell(4, 7, 'offwhite')
+cells['5a'] = new Cell(5, 0, 'offwhite')
+cells['5b'] = new Cell(5, 1, 'feltgreen')
+cells['5c'] = new Cell(5, 2, 'offwhite')
+cells['5d'] = new Cell(5, 3, 'feltgreen')
+cells['5e'] = new Cell(5, 4, 'offwhite')
+cells['5f'] = new Cell(5, 5, 'feltgreen')
+cells['5g'] = new Cell(5, 6, 'offwhite')
+cells['5h'] = new Cell(5, 7, 'feltgreen')
+cells['6a'] = new Cell(6, 0, 'feltgreen')
+cells['6b'] = new Cell(6, 1, 'offwhite')
+cells['6c'] = new Cell(6, 2, 'feltgreen')
+cells['6d'] = new Cell(6, 3, 'offwhite')
+cells['6e'] = new Cell(6, 4, 'feltgreen')
+cells['6f'] = new Cell(6, 5, 'offwhite')
+cells['6g'] = new Cell(6, 6, 'feltgreen')
+cells['6h'] = new Cell(6, 7, 'offwhite')
+cells['7a'] = new Cell(7, 0, 'offwhite')
+cells['7b'] = new Cell(7, 1, 'feltgreen')
+cells['7c'] = new Cell(7, 2, 'offwhite')
+cells['7d'] = new Cell(7, 3, 'feltgreen')
+cells['7e'] = new Cell(7, 4, 'offwhite')
+cells['7f'] = new Cell(7, 5, 'feltgreen')
+cells['7g'] = new Cell(7, 6, 'offwhite')
+cells['7h'] = new Cell(7, 7, 'feltgreen')
+
+
+
+
+function createUI() {
+
+
+    let table = "<table border='1'>"
+    
+    for ( let k in cells ) {
+        const obj = cells[k]
+        if ( obj.col === 0 ) {
+            table += "<tr>"
+        }
+        table += "<td>" + obj.getHtml() + "</td>"
+
+        if ( obj.col === 7 ) {
+            table += "</tr>"
+        }
     }
     table += "</table>"
     document.getElementById("board").innerHTML = table 
-    console.log( table )
-
 }
