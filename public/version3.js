678dff7283571669b5e6bc5fff8b6e927a8b96fc
Paul Montgomery <finch.wren.swift@gmail.com>
2024-12-09 21:11:09 -0800

    MOnday 7

--- public/version3.js
+++ public/version3.js
@@ -2,10 +2,10 @@ function getPossibleMoves(type) {
     let moves = [];
     switch (type) {
         case 'rook':
-            moves = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+            moves = [[-1,0], [1,0],  [0,-1], [0,1]]
             break;
         case 'knight':
-            moves = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
+            moves = [[2, 1],[2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
             break;
         case 'bishop':
             moves = [[1, 1], [1, -1], [-1, 1], [-1, -1]]
@@ -20,28 +20,47 @@ function getPossibleMoves(type) {
     return moves;
 }
 
+function yellow(msg) {
+    console.log("%c " + msg, "background-color:yellow")
+}
+function blue(msg) {
+    console.log("%c " + msg, "background-color:lightblue")
+}
+
+function calculateMoves(row, col, moves, recurse) {
+    function checkit(r1, c1, r2, c2, limit, accumulator) {
 
 
-function calculateMoves(row, col, moves, recurse, visited = new Set()) {
+        if (limit < 1) {
+            return accumulator;
+        }
+        const y = r1 + r2;
+        const x = c1 + c2;
+        if (y >= 0 && y < 8 && x >= 0 && x < 8) {
+            const id = `${y}-${x}`;
+            if (!accumulator.includes(id)) {
 
-    if (recurse === 0) return []; // Base case: no more moves left
 
-    let possibleCells = [];
-    visited.add(`${row}-${col}`); // Mark current cell as visited
-
-    for (const [dr, dc] of moves) {
-        const newRow = row + dr;
-        const newCol = col + dc;
-        const cellId = `${newRow}-${newCol}`;
-
-        // Ensure within bounds and not already visited
-        if (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8 && !visited.has(cellId)) {
-            possibleCells.push(cellId); // Add valid move
-            possibleCells.push(
-                ...calculateMoves(newRow, newCol, moves, recurse - 1, visited) // Recur to find further moves
-            );
+                const potentialPiece = document.getElementById(id).getAttribute('data-piece')
+                if ( potentialPiece == undefined || potentialPiece.length < 1 ) {
+                    // Avoid moving untop another piece. 
+                    // Attacks will be dealt with seperately ( mostly because pawns are tricky )
+                    accumulator.push(id);
+                    checkit(y, x, r2, c2, limit - 1, accumulator);    
+                } else {
+                    return accumulator
+                }
+            }
         }
     }
 
-    return possibleCells;
+    let accumulator = [];
+    for (let i = 0; i < moves.length; i++) { 
+        const arr = moves[i];
+        const rowDirection = arr[0];
+        const colDirection = arr[1];
+        checkit(row, col, rowDirection, colDirection, recurse, accumulator);
+    }
+    console.log(accumulator);
+    return accumulator;
 }
